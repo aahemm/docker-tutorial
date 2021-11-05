289e65aac71560004e72c06e4e0ff70212b9e0ca
aliakbar <aliakbar@hamravesh.com>
2021-11-05 20:00:03 +0330

    Add docker-compose file

--- qops/settings.py
+++ qops/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'qops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     },
 }
 
