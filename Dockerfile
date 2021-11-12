a33cae39c12cd13c0a88f9dc9304b9bf66297859
aliakbar <aliakbar@hamravesh.com>
2021-11-12 13:52:39 +0330

    Use sqlite db

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM python:3.7
 ENV PYTHONUNBUFFERED 1
 RUN mkdir /code
-WORKDIR /code
 COPY requirements.txt /code/
+WORKDIR /code
 RUN pip install -r requirements.txt
+
 COPY . /code/
 ENTRYPOINT ["/bin/bash", "start.sh"]
--- qops/settings.py
+++ qops/settings.py
@@ -99,11 +99,7 @@ WSGI_APPLICATION = 'qops.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': BASE_DIR / 'db.sqlite3'
     },
 }
 
